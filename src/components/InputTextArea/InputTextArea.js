4b7d6a7422ad1eb9d0dfe37e2c4260d2989751df
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-09 21:05:15 -0500

    cleaning up textarea component

--- src/components/InputTextArea/InputTextArea.js
+++ src/components/InputTextArea/InputTextArea.js
@@ -3,7 +3,16 @@ import React from 'react';
 const InputTextArea = ({ extraClass, id, label, name, placeholder, required, disabled, length, readonly }) => (
   <div className={`textarea ${extraClass}`}>
     <label className={`textarea__label`} for={id} >{label}</label>
-    <textarea className={`textarea__input`} id={id} name={name} placeholder={placeholder} maxlength={length} spellcheck="true" required disabled readonly />
+    <textarea 
+      className={`textarea__input`} 
+      id={id} 
+      name={name} 
+      placeholder={placeholder} 
+      maxlength={length} 
+      spellcheck="true" 
+      required 
+      disabled 
+      readonly />
   </div>
 ) 
 
