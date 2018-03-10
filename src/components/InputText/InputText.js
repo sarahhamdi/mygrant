735480b4604ab77236ae5f1907b985812c04e790
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-09 20:50:41 -0500

    updating inputtext component with more attributes

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const InputText = ({ extraClass, id, label, name, placeholder }) => (
+const InputText = ({ extraClass, id, label, name, placeholder, required, pattern}) => (
   <div className={`inputext ${extraClass}`}>
-    <label for={id}>{label}</label>
-    <input type="text" id={id} name={name} placeholder={placeholder}/>
+    <label className={`inputext__label`} for={id} >{label}</label>
+    <input className={`inputext__input`} type="text" id={id} name={name} placeholder={placeholder} required pattern={pattern} />
+    <span className="validity"></span>
   </div>
 ) 
 
 export default InputText;
-
