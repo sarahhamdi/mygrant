0212fcec0bca94d66b16822ba98d6c63666af282
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-09 21:06:16 -0500

    cleanup inputtext component

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const InputText = ({ extraClass, id, label, type, name, placeholder, required, pattern}) => (
   <div className={`inputext ${extraClass}`}>
-    <label className={`inputext__label`} for={id} >{label}</label>
-    <input className={`inputext__input`} type={type} id={id} name={name} placeholder={placeholder} required pattern={pattern} />
+    <label className={`inputext__label`} for={id}>{label}</label>
+    <input 
+      className={`inputext__input`} 
+      type={type} 
+      id={id} 
+      name={name} 
+      placeholder={placeholder} 
+      pattern={pattern}
+      required />
     <span className="validity"></span>
   </div>
 ) 
