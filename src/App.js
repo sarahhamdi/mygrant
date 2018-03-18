8af1f7f36a86a7fb6711afeac0f8461d9d3a9d98
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-18 16:24:49 -0400

    setting up routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './assets/large-logo.svg';
 import add from './assets/icon-add.svg';
 import addLight from './assets/icon-add-light.svg';
 import axios from 'axios';
+import { 
+  BrowserRouter as Router, 
+  Route, Link } from 'react-router-dom';
 
 // components
 import Icon from './components/Icon/Icon';
@@ -22,6 +25,44 @@ import Header from './components/Header/Header';
 import FormLogin from './components/FormLogin/FormLogin';
 import FormRegistration from './components/FormRegistration/FormRegistration';
 
+const registration = () => (
+  <main className="page__registration">
+    <Image src={logo} />
+    <FormRegistration name="form-registration" id="form-registration"/>
+  </main>
+);
+
+const login = () => (
+  <main className="page__login">
+    <Image src={logo} />
+    <FormLogin name="form-login" id="form-login"/>
+    <a href="/registration">register here</a>
+  </main>
+);
+
+const grants = () => (
+  <main className="page__grants">
+    <Header />
+  </main>
+);
+
+const myDetails = () => (
+  <main className="page__my-details">
+    <Header />
+  </main>
+);
+
+const data = () => (
+  <main className="page__data">
+    <Header />
+  </main>
+);
+
+const account = () => (
+  <main className="page__account">
+    <Header />
+  </main>
+);
 
 class App extends Component {
   // this is the optimal place to 
@@ -39,30 +80,40 @@ class App extends Component {
       .get('/hello')
       .then(console.log);
   }
+
   render() {
     return (
-      <div className="App">
-        <Header />
-        <H1 text="Random Components" />
-        <Image src={logo} />
-        <H1 text="Header 1" />
-        <H2 text="Header 2" />
-        <H3 text="Header 3" />
-        <Paragraph text="Sphinx of black quartzzz, judge my vow" />
-        <Icon type={add} />
-        <Button text="a button"/>
-        <ButtonWithIcon type={addLight} text="button with icon" />
-        <InputText name="newtextform" placeholder="some placeholder text" label="a text input" id="newInputText"/>
-        <InputTextArea name="newtextform" placeholder="some placeholder text" label="a text input" id="newInputText" />
-
-        <H1 text="Registration Form" />
-        <FormRegistration name="form-registration" id="form-registration"/>
-
-        <H1 text="Login Form" />
-        <FormLogin name="form-login" id="form-login"/>
-      </div>
+        <Router className="App">
+          <React.Fragment>
+            <Route exact path='/' component={login} />
+            <Route exact path='/registration' component={registration} />
+            <Route path='/grants' component={grants} />
+            <Route exact path='/my-details' component={myDetails} />
+            <Route exact path='/data' component={data} />
+            <Route exact path='/account' component={account} />
+          </React.Fragment>
+        </Router>
     );
   }
 }
 
 export default App;
+
+        // <Header />
+        // <H1 text="Random Components" />
+        // <Image src={logo} />
+        // <H1 text="Header 1" />
+        // <H2 text="Header 2" />
+        // <H3 text="Header 3" />
+        // <Paragraph text="Sphinx of black quartzzz, judge my vow" />
+        // <Icon type={add} />
+        // <Button text="a button"/>
+        // <ButtonWithIcon type={addLight} text="button with icon" />
+        // <InputText name="newtextform" placeholder="some placeholder text" label="a text input" id="newInputText"/>
+        // <InputTextArea name="newtextform" placeholder="some placeholder text" label="a text input" id="newInputText" />
+
+        // <H1 text="Registration Form" />
+        // <FormRegistration name="form-registration" id="form-registration"/>
+
+        // <H1 text="Login Form" />
+        // <FormLogin name="form-login" id="form-login"/>
