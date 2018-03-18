21a744c9a577ffa49b37462baff908e68df5b68d
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-17 23:56:50 -0400

    fixing some typography styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,9 +11,9 @@ const Header = () => (
         <li className="header__nav-list-item header__logo">
           <Image src={logo} />
         </li>
-        <li className="header__nav-list-item header__nav-list-item"><h3>Org Details</h3></li>
-        <li className="header__nav-list-item"><h3>Grants</h3></li>
-        <li className="header__nav-list-item"><h3>Data</h3></li>
+        <li className="header__nav-list-item header__nav-list-item"><h2 className="header-3">Org Details</h2></li>
+        <li className="header__nav-list-item"><h2 className="header-3">Grants</h2></li>
+        <li className="header__nav-list-item"><h2 className="header-3">Data</h2></li>
         <li className="header__nav-list-item header__account-icon">
           <Icon type={add}/>
         </li>
