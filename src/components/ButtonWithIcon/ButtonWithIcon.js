e30364284b097ce50b001aec20a10aa0a6613dcc
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-17 23:22:24 -0400

    styled button with icon component

--- src/components/ButtonWithIcon/ButtonWithIcon.js
+++ src/components/ButtonWithIcon/ButtonWithIcon.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Icon from '../Icon/Icon';
 
 
-const ButtonWithIcon = ({ text, action, extraClass, type }) => ( <button className={`button ${extraClass}`} onChange={() => action}><Icon type={type} />{text}</button>
+const ButtonWithIcon = ({ text, action, extraClass, type }) => ( 
+  extraClass ? 
+  <button className={`button button---icon ${extraClass}`} onChange={() => action}><Icon type={type} />{text}</button>
+  : <button className={`button button--icon`} onChange={() => action}><Icon type={type} />{text}</button>
+  
 )
 
 export default ButtonWithIcon;
