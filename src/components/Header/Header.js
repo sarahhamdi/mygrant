1ef942e406cfcddf45fc733ebcec7b2496123af6
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-17 23:54:33 -0400

    worked on header component styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Image from '../Image/Image';
 import Icon from '../Icon/Icon';
-import logo from '../../assets/large-logo.svg'
+import logo from '../../assets/small-logo.svg'
 import add from '../../assets/icon-add.svg'
 
 const Header = () => (
   <header className="header">
     <nav className="header__nav">
-      <ul className="header__nav-ist">
+      <ul className="header__nav-list">
         <li className="header__nav-list-item header__logo">
           <Image src={logo} />
         </li>
-        <li className="header__nav-list-item header__nav-list-item">Org Details</li>
-        <li className="header__nav-list-item">Grants</li>
-        <li className="header__nav-list-item">Data</li>
+        <li className="header__nav-list-item header__nav-list-item"><h3>Org Details</h3></li>
+        <li className="header__nav-list-item"><h3>Grants</h3></li>
+        <li className="header__nav-list-item"><h3>Data</h3></li>
         <li className="header__nav-list-item header__account-icon">
           <Icon type={add}/>
         </li>
