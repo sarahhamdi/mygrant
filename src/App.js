c543acb81a68d81f0166e81f58de0fe1034f24e5
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-21 12:21:32 -0400

    added more endpoints

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import { 
   BrowserRouter as Router, 
   Route } from 'react-router-dom';
@@ -11,38 +10,17 @@ import PageLogin from './PageLogin'
 import PageData from './PageData'
 import PageAccount from './PageAccount'
 
-class App extends React.Component {
-  // this is the optimal place to 
-  // make an http request
-  componentDidMount() { 
-    // axios is a promise-based api
-    // so you can chain .then and use callback for response
-    // make sure you on the CORRECT PORT (ie not 3000)
-    // do not make a get request to a specific domain 
-    // e.g.  axios.get('localhost:8080/hello')
-    // because you will get a CORs error
-    // AND because you may not be using that port all the time
-    // use a proxy! add "proxy": "http://localhost:8080" to package.json
-    axios
-      .get('/hello')
-      .then(console.log);
-  }
-
-  render() {
-    return (
-        <Router className="App">
-          <React.Fragment>
-            <Route exact path='/' component={PageLogin} />
-            <Route path='/login' component={PageLogin} />
-            <Route path='/register' component={PageRegistration} />
-            <Route path='/grants' component={PageGrants} />
-            <Route path='/org-details' component={PageOrgDetails} />
-            <Route path='/data' component={PageData} />
-            <Route path='/account' component={PageAccount} />
-          </React.Fragment>
-        </Router>
-    );
-  }
-}
-
+const App = () => (
+  <Router className="App">
+    <React.Fragment>
+      <Route exact path='/' component={PageLogin} />
+      <Route path='/login' component={PageLogin} />
+      <Route path='/register' component={PageRegistration} />
+      <Route path='/grants' component={PageGrants} />
+      <Route path='/org-details' component={PageOrgDetails} />
+      <Route path='/data' component={PageData} />
+      <Route path='/account' component={PageAccount} />
+    </React.Fragment>
+  </Router>
+)
 export default App;
--- src/PageGrants.js
+++ src/PageGrants.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import addLight from './assets/icon-add-light.svg';
 import Header from './components/Header/Header';
@@ -7,29 +8,63 @@ import FormGrant from './components/FormGrant/FormGrant';
 
 class PageGrants extends React.Component {
   state = {
-    grants: ['test 1', 'test 2']
+    grants: [],
+    visible: false
   }
 
   refresh = () => {
-    // get statement
-    // if successful
-    // setstate to new/all data
+    // axios is a promise-based api
+    // so you can chain .then and use callback for response
+    // make sure you on the CORRECT PORT (ie not 3000)
+    // do not make a get request to a specific domain 
+    // e.g.  axios.get('localhost:8080/hello')
+    // because you will get a CORs error
+    // AND because you may not be using that port all the time
+    // use a proxy! add "proxy": "http://localhost:8080" to package.json
+    axios
+    .get('/grants')
+    .then(res => {
+      console.log(res.data.payload)
+      const grants = res.data.payload;
+      this.setState({
+        grants
+      })
+    });
   }
 
-  componentDidMount() {
+  showForm = e => {
+    e.preventDefault();
+
+    this.setState({
+      visible: true
+    })
+  }
+    
+  componentDidMount() { 
     this.refresh();
   }
+
 
   render() {
     return (
       <main className="page__grants">
       <Header />
-      <ButtonWithIcon type={addLight} text="Add a Grant" />
-      <FormGrant name="form-grants" id="form-grants" />
+      <ButtonWithIcon type={addLight} text="Add a Grant" action={this.showForm} />
 
-      {this.state.grants.map((grant, i) => (
-        <div key="i">
-          {grant}
+      {this.state.visible ?
+        <FormGrant name="form-grants" id="form-grants" />
+        : null}
+      
+
+      {this.state.grants.map(grant => (
+        <div key={grant._id}>
+          {grant.name}
+          {grant.amount}
+          {grant.grantLink}
+          {grant.granted}
+          {grant.due}
+          {grant.status}
+          {grant.tags.map(tag => (tag))}
         </div>
       ))}
 
