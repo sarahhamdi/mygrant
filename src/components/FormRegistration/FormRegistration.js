918c4e33912ac602887420202ba33cf6a92c83b4
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-21 18:16:18 -0400

    made new endpoints for users and ability to add users from form

--- src/components/FormRegistration/FormRegistration.js
+++ src/components/FormRegistration/FormRegistration.js
@@ -1,37 +1,80 @@
 import React from 'react';
 import InputText from '../InputText/InputText'
 import Button from '../Button/Button'
+import axios from 'axios';
 
-const FormRegistration= ({ name, id }) => (
-  <form name={name} id={id} method="post" className="form--registration">
-    <InputText 
-      type="text"
-      name="user-name" 
-      placeholder="Severus Snape" 
-      label="Your Name" 
-      id="registration--input-name"/>
-    <InputText 
-      type="text"
-      name="user-org" 
-      placeholder="The Order of the Phoenix" 
-      label="Your Organization's Name" 
-      id="registration--input-org-name"/>
-    <InputText 
-      type="email"
-      name="Your Email" 
-      placeholder="ssnape@hogwarts.edu" 
-      label="Your Email" 
-      pattern="/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/"
-      id="registration--input-nemail"/>
-    <InputText 
-      type="password"
-      name="user-password" 
-      placeholder="30+ characters please!" 
-      label="Your Password" 
-      id="registration--input-password"
-      required="true" />
-    <Button text="Create Account"/>
-  </form>
-)
+class FormRegistration extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    organization: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  postNewUser = () => {
+    const { firstName, lastName, email, password, organization } = this.state;
+    axios
+      .post('/users', { firstName, lastName, email, password, organization })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    const { name, id } = this.props;
+    return (
+      <form name={name} id={id} onSubmit={this.postNewUser} className="form--registration">
+        <InputText 
+          type="text"
+          name="firstName" 
+          placeholder="Severus Snape" 
+          label="Your First Name" 
+          id="registration--input-name-1"
+          handleChange={this.handleChange} />
+        <InputText 
+          type="text"
+          name="lastName" 
+          placeholder="Severus Snape" 
+          label="Your Last Name" 
+          id="registration--input-name-2"
+          handleChange={this.handleChange} />
+        <InputText 
+          type="text"
+          name="organization" 
+          placeholder="The Order of the Phoenix" 
+          label="Your Organization's Name" 
+          id="registration--input-org-name"
+          handleChange={this.handleChange} />
+        <InputText 
+          type="email"
+          name="email" 
+          placeholder="ssnape@hogwarts.edu" 
+          label="Your Email" 
+          pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+          id="registration--input-nemail"
+          handleChange={this.handleChange} />
+        <InputText 
+          type="password"
+          name="password" 
+          placeholder="30+ characters please!" 
+          label="Your Password" 
+          id="registration--input-password"
+          required="true" 
+          handleChange={this.handleChange} />
+        <Button text="Create Account"/>
+      </form>
+    )
+  }
+} 
 
 export default FormRegistration; 
