2fbf6dbfbbaa46a32a4de18decfe0c37b31b5199
Sarah Hamdi <s.hamdi@gmail.com>
2018-03-21 23:06:35 -0400

    adding orgdetail component

---
+++ src/components/OrgDetail/OrgDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const OrgDetail = ({ title, text }) => {
+  <div className="org-detail">
+    <h3 className="header-3 org-detail__title">{title}</h3>
+    <p className="paragraph org-detail__text">{text}</p>
+  </div>
+}
+
+export default OrgDetail;
